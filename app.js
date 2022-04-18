435cb1285956970d8c9704b15b2ded22505f8597
Hiren Patel <patelhiren.hp19@gmail.com>
2022-04-18 10:19:54 +0530

    introducing componets

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
     data() {
         return {
-            detailsAreVisible: false,
             friends: [{
                     id: 'Hiren',
                     name: 'Hiren Patel',
@@ -16,12 +15,34 @@ const app = Vue.createApp({
                 },
             ],
         };
+    }
+});
+
+app.component('frined-contact', {
+    template: `
+        <li>
+            <h2>{{ friend.name }}</h2>
+            <button @click="toggleDetails(friend.id)">{{ detailsAreVisible ? 'Hide' : 'Show'}} Details</button>
+            <ul v-if="detailsAreVisible">
+                <li><strong>Phone:</strong> {{ friend.phone }}</li>
+                <li><strong>Email:</strong> {{ friend.email }}</li>
+            </ul>
+        </li>`,
+    data() {
+        return {
+            detailsAreVisible: false,
+            friend: {
+                id: 'Hiren',
+                name: 'Hiren Patel',
+                phone: '+919726977958',
+                email: 'patelhiren.hp19@gmail.com',
+            },
+        };
     },
     methods: {
         toggleDetails() {
             this.detailsAreVisible = !this.detailsAreVisible;
-        }
+        },
     },
 });
-
 app.mount('#app');
